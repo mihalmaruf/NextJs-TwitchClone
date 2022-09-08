669b22104d8cbdcf46369d444941fd14e9aa46f2
Mihal <marufmihal43@gmail.com>
2022-09-08 12:20:56 -0400

    first commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head' 
+import Layout from '../components/Layout'
+import Main from '../components/Main'
+
+export default function Home() {
+  return (
+    <div>
+      <Layout />
+      <Main />
+    </div>
+  )
+}
